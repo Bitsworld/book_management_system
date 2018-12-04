bdf9a1564390758a28b0ec8f3c7efda5b82fc9a5
Bitsworld <2264915345@qq.com>
2018-12-04 22:53:12 +0800

    用户管理功能全部完成

--- UserManage.py
+++ UserManage.py
@@ -2,10 +2,11 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-# from AddUserDialog import AddUserDialog
+from AddUserDialog import AddUserDialog
 # from DropUserDialog import DropUserDialog
 from UserStorageView import UserStorageView
-# from ReviseUserDialog import ReviseUserDialog
+from ReviseUserDialog import ReviseUserDialog
+
 
 class UserManage(QWidget):
     def __init__(self,  parent=None):
@@ -36,7 +37,7 @@ class UserManage(QWidget):
         self.revise_user_button.setFixedWidth(100)
         self.revise_user_button.setFixedHeight(40)
 
-        self.not_back_user_button = QPushButton('未归还读者')
+        self.not_back_user_button = QPushButton('超期读者')
         self.not_back_user_button.setFont(font)
         self.not_back_user_button.setFixedWidth(100)
         self.not_back_user_button.setFixedHeight(40)
@@ -55,39 +56,35 @@ class UserManage(QWidget):
         self.revise_user_button.clicked.connect(self.revise_user_button_clicked)
         self.not_back_user_button.clicked.connect(self.not_back_user_button_clicked)
 
+
         self.setWindowTitle('读者管理')
         self.setGeometry(100, 100, 900, 600)
         self.setWindowIcon(QIcon("./imagesSourse/book_icon2.png"))
         self.show()
 
     def add_user_button_clicked(self):
-        pass
-        '''
-        add_dialog = AddUserDialog(self)
+        add_user_dialog = AddUserDialog(self)
         # add_dialog.exec_()
-        add_dialog.add_user_success_signal.connect(self.storage_view.search_button_clicked)
-        add_dialog.show()
-        '''
+        add_user_dialog.add_user_success_signal.connect(self.storage_view.search_button_clicked)
+        add_user_dialog.show()
 
     def drop_user_button_clicked(self):
-        pass
-        '''
-        drop_dialog = DropUserDialog(self)
-        drop_dialog.drop_user_successful_signal.connect(self.storage_view.search_button_clicked)
-        drop_dialog.drop_back_successful_signal.connect(self.storage_view.search_button_clicked)
-        drop_dialog.show()
-        '''
+        self.storage_view.drop_user()
 
     def revise_user_button_clicked(self):
-        pass
-        '''
-        revise_dialog = ReviseUserDialog(self)
-        revise_dialog.revise_successful_signal.connect(self.storage_view.search_button_clicked)
-        revise_dialog.revise_back_signal.connect(self.storage_view.search_button_clicked)
-        revise_dialog.show()
-        '''
+        select_user_id = self.storage_view.get_revise_user_id()
+        print(select_user_id)
+        if select_user_id == None:
+            print(QMessageBox.information(self, '提示', '请选择要修改的用户', QMessageBox.Yes, QMessageBox.Yes))
+        else:
+            revise_dialog = ReviseUserDialog(select_user_id, self)
+            self.storage_view.search_button_clicked()
+            revise_dialog.revise_user_successful_signal.connect(self.storage_view.search_button_clicked)
+            # revise_dialog.revise_back_signal.connect(self.storage_view.search_button_clicked)
+            revise_dialog.show()
+
     def not_back_user_button_clicked(self):
-        pass
+        self.storage_view.no_back_book_user()
 
 
 if __name__ == '__main__':
